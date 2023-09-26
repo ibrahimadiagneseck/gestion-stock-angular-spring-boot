7f361975af127474dd473e0e0af2b54c7bb4a0a8
ibrahima diagne seck <ibrahimadiagneseck@esp.sn>
2023-09-26 11:45:44 +0000

    commit

--- gestion-stock-postgres/src/main/java/sn/douanes/gestionstockpostgres/api/UtilisateurVehiculeController.java
+++ gestion-stock-postgres/src/main/java/sn/douanes/gestionstockpostgres/api/UtilisateurVehiculeController.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
-public class UserVehiculeController {
 
     @Autowired
     UtilisateurVehiculeRepository userVehiculeRepository;
