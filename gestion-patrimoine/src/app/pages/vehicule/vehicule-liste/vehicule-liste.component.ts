cf66ad1f3f39735b35b074d132632ce6123232ce
ibrahima diagne seck <ibrahimadiagneseck@esp.sn>
2023-10-02 03:03:06 +0000

    commit

--- gestion-patrimoine/src/app/pages/vehicule/vehicule-liste/vehicule-liste.component.ts
+++ gestion-patrimoine/src/app/pages/vehicule/vehicule-liste/vehicule-liste.component.ts
@@ -4,11 +4,11 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { VehiculeAjouterComponent } from '../vehicule-ajouter/vehicule-ajouter.component';
 import { VehiculeDetailComponent } from '../vehicule-detail/vehicule-detail.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { ServicesService } from 'src/app/services/services.service';
 import { IVehicule } from 'src/app/models/vehicule';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { Observable, Subject, debounceTime, distinctUntilChanged, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-interface1',
@@ -17,9 +17,14 @@ import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } fr
 })
 export class VehiculeListeComponent implements OnInit, AfterViewInit {
 
+  focusOnInput: boolean = true;
+  dataAvailable: boolean = true;
+
+
+
   // rechercher
   searchTerms = new Subject<string>();
-  vehicules$!: Observable<IVehicule[]>;
+  vehicules$: Observable<IVehicule[]> = of();
 
 
   vehicules: IVehicule[] = [];
@@ -37,6 +42,10 @@ export class VehiculeListeComponent implements OnInit, AfterViewInit {
     private el: ElementRef, private renderer: Renderer2
   ) { }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   recupererVehicules() {
     this.servicesService.getVehicules().subscribe({
@@ -77,6 +86,63 @@ export class VehiculeListeComponent implements OnInit, AfterViewInit {
 
 
   ngAfterViewInit() {
+
+    const searchInput = document.getElementById('searchInput') as HTMLInputElement;
+    const suggestionList = document.getElementById('suggestionList');
+
+    // Liste de suggestions (peut être dynamique en fonction de votre application)
+    const suggestions = [
+      'Proposition 1',
+      'Proposition 2',
+      'Proposition 3',
+      'Proposition 4',
+      'Proposition 5',
+    ];
+
+    // Fonction pour mettre à jour la liste de suggestions
+    function updateSuggestions() {
+      const inputValue = searchInput.value.toLowerCase();
+      suggestionList!.innerHTML = '';
+
+      // suggestions.forEach(suggestion => {
+      //   if (suggestion.toLowerCase().includes(inputValue)) {
+      //     const li = document.createElement('li');
+      //     li.textContent = suggestion;
+      //     suggestionList!.appendChild(li);
+      //   }
+      // });
+
+      // if (suggestionList !== null) {
+      //   if (suggestionList.childElementCount === 0) {
+      //     suggestionList!.style.display = 'none';
+      //   } else {
+      //     suggestionList!.style.display = 'block';
+      //   }
+      // }
+
+
+      // Si la zone de recherche est vide, masquer la liste de suggestions
+      if (inputValue === '') {
+        suggestionList!.style.display = 'none';
+      } else {
+        suggestionList!.style.display = 'block';
+      }
+    }
+
+    // Écouteur d'événement pour la saisie dans la zone de recherche
+    searchInput.addEventListener('input', updateSuggestions);
+
+    // Écouteur d'événement pour le blur de la zone de recherche
+    searchInput.addEventListener('blur', () => {
+      // Masquer la liste de suggestions lorsque l'input perd le focus
+      suggestionList!.style.display = 'none';
+    });
+
+
+
+
+
+    // menu
     const coll = this.el.nativeElement.getElementsByClassName("collapsible");
     for (let i = 0; i < coll.length; i++) {
       this.renderer.listen(coll[i], "click", () => {
@@ -90,21 +156,15 @@ export class VehiculeListeComponent implements OnInit, AfterViewInit {
       });
     }
 
-
-
-    // this.dataSource.paginator = this.paginator;
-    // setTimeout(() => {
-    //   this.dataSource.paginator = this.paginator;
-    // }, 2000);
   }
 
   popupAjouter() {
     this.matDialog.open(
       VehiculeAjouterComponent,
       {
-        width:'75%',
-        enterAnimationDuration:'1000ms',
-        exitAnimationDuration:'2000ms',
+        width: '75%',
+        enterAnimationDuration: '1000ms',
+        exitAnimationDuration: '2000ms',
         data: {
           concat: []
         }
@@ -116,9 +176,9 @@ export class VehiculeListeComponent implements OnInit, AfterViewInit {
     this.matDialog.open(
       VehiculeDetailComponent,
       {
-        width:'75%',
-        enterAnimationDuration:'1000ms',
-        exitAnimationDuration:'2000ms',
+        width: '75%',
+        enterAnimationDuration: '1000ms',
+        exitAnimationDuration: '2000ms',
         data: {
           element
         }
@@ -139,26 +199,6 @@ export class VehiculeListeComponent implements OnInit, AfterViewInit {
     "couleur"
   ];
 
-  // "id"
-  // "numeroChassis"
-  // "numeroMatricule"
-  // "modele"
-  // "marque"
-  // "transmission"
-  // "couleur"
-  // "dateFabrication"
-  // "dateCommande"
-  // "dateLivraison"
-  // "energie"
-  // "etat"
-  // "typeVehicule"
-
-
-  // dataSource = new MatTableDataSource<IVehicule>(ELEMENT_DATA);
-  // dataSource = new MatTableDataSource<IVehicule>(this.vehicules);
-
-
-  // @ViewChild(MatPaginator) paginator!: MatPaginator;
 
 }
 
